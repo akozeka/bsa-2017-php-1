7381a0dce93b3e991b1661ceaf30dd63450ba7bb
Alexey Kozeka <kozeka.alex@gmail.com>
2017-05-24 11:47:01 +0300

    Added emoji generator implementation.

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -6,6 +6,10 @@ class EmojiGenerator
 {
     public function generate()
     {
-        //TODO: Implement emoji generator using yield
+        $emojiUnicodeCodepoints = ['1f600', '1f603', '1f604', '1f601', '1f606', '1f605', '1f602'];
+
+        foreach ($emojiUnicodeCodepoints as $codepoint) {
+            yield mb_convert_encoding("&#x{$codepoint};", 'UTF-8', 'HTML-ENTITIES');
+        }
     }
 }
