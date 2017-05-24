233ebcafedc34ea7d3be60e59fa3764da4bd79f0
Alexey Kozeka <kozeka.alex@gmail.com>
2017-05-24 14:21:50 +0300

    Reimplemented emoji generator using PHP7's hexadecimal unicode codepoint syntax.

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -4,12 +4,20 @@ namespace BinaryStudioAcademy\Task2;
 
 class EmojiGenerator
 {
+    const EMOJIS = [
+        "\u{1f600}",
+        "\u{1f603}",
+        "\u{1f604}",
+        "\u{1f601}",
+        "\u{1f606}",
+        "\u{1f605}",
+        "\u{1f602}",
+    ];
+
     public function generate()
     {
-        $emojiUnicodeCodepoints = ['1f600', '1f603', '1f604', '1f601', '1f606', '1f605', '1f602'];
-
-        foreach ($emojiUnicodeCodepoints as $codepoint) {
-            yield mb_convert_encoding("&#x{$codepoint};", 'UTF-8', 'HTML-ENTITIES');
+        foreach (self::EMOJIS as $emoji) {
+            yield $emoji;
         }
     }
 }
